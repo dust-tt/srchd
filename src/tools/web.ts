c80013850d421cc2d633e729d984d7d4f5116f4e
Anas Lecaillon <anas@dust.tt>
2025-11-05 15:48:06 +0100

    put-token-limit

--- src/tools/web.ts
+++ src/tools/web.ts
@@ -33,13 +33,16 @@ export async function createWebServer(): Promise<McpServer> {
       });
 
       if (scrapeResponse.success) {
-        // console.log("WEBPAGE CONTENT:", scrapeResponse.markdown);
+        const text = scrapeResponse.markdown
+          ? scrapeResponse.markdown.slice(0, 40000) // Approximately 10k tokens.
+          : "";
+        console.log("WEBPAGE CONTENT:", text);
         return {
           isError: false,
           content: [
             {
               type: "text",
-              text: scrapeResponse.markdown ?? "",
+              text,
             },
           ],
         };
@@ -72,7 +75,7 @@ export async function createWebServer(): Promise<McpServer> {
         for (const [i, res] of searchResponse.data.entries()) {
           results += `${i + 1}. [${res.title}](${res.url})\n${res.description}\n\n`;
         }
-        // console.log("SEARCH RESULTS:\n", results);
+        console.log("SEARCH RESULTS:\n", results);
         return {
           isError: false,
           content: [
