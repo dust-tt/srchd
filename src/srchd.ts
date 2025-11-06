9e5429d79a232360e5a4bac7297dd6807643a12c
Anas Lecaillon <anas@dust.tt>
2025-11-06 13:32:22 +0100

    orthogonolisation

--- src/srchd.ts
+++ src/srchd.ts
@@ -138,7 +138,38 @@ tokensMetric
       );
     }
 
-    const metrics = await Metrics.tokens(experimentRes);
+    const metrics = await Metrics.experimentTokens(experimentRes);
+    if (!metrics) {
+      return exitWithError(
+        new Err(
+          new SrchdError(
+            "not_found_error",
+            `Experiment '${experiment}' not found.`,
+          ),
+        ),
+      );
+    }
+
+    console.table([metrics]);
+  });
+
+tokensMetric
+  .command("full")
+  .argument("<experiment>", "Experiment name")
+  .action(async (experiment) => {
+    const experimentRes = await ExperimentResource.findByName(experiment);
+    if (!experimentRes) {
+      return exitWithError(
+        new Err(
+          new SrchdError(
+            "not_found_error",
+            `Experiment '${experiment}' not found.`,
+          ),
+        ),
+      );
+    }
+
+    const metrics = await Metrics.fullTokens(experimentRes);
     if (!metrics) {
       return exitWithError(
         new Err(
@@ -157,7 +188,7 @@ tokensMetric
       },
     ]);
     const agents = [];
-    for (const { name, usage } of Object.values(metrics.agentsTokenUsage)) {
+    for (const [name, usage] of Object.entries(metrics.agentsTokenUsage)) {
       agents.push({ name, ...usage });
     }
     console.table(agents);
